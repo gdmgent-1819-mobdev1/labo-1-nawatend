cbaf685030825e5263971964a9e55fda4dddb8f2
nawatend <32384185+nawatend@users.noreply.github.com>
2018-09-27 17:10:31 +0200

    Entry 3

--- Random Arcade/js/index.js
+++ Random Arcade/js/index.js
@@ -1,6 +1,6 @@
 let arcadeCharacter = [];
 
-let charbox = document.getElementsByClassName("charbox")[0];
+let charbox = document.getElementsByClassName("charbox");
 let randomNr;
 for (let i = 0; i < 7; i++) {
   charbox.innerHTML += "<br>";
@@ -13,6 +13,21 @@ for (let i = 0; i < 7; i++) {
     } else {
       div.setAttribute("class", "smallblackbox");
     }
-    charbox.appendChild(div);
+    charbox[0].appendChild(div);
+  }
+}
+
+for (let i = 0; i < 7; i++) {
+  charbox.innerHTML += "<br>";
+  for (let n = 0; n < 7; n++) {
+    let div = document.createElement("div");
+    randomNr = Math.floor(Math.random() * 2);
+    console.log(randomNr);
+    if (randomNr != 0) {
+      div.setAttribute("class", "whitebox");
+    } else {
+      div.setAttribute("class", "smallblackbox");
+    }
+    charbox[1].appendChild(div);
   }
 }
